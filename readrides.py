5848bbcba372fdc1fe398439e1c6576d4b955f39
Pochingto <rcwk3756@gmail.com>
2024-01-15 18:55:41 -0500

    exercise 2.1

---
+++ readrides.py
@@ -0,0 +1,121 @@
+import csv
+
+def read_rides_as_tuples(filename):
+    '''
+    Read the bus ride data as a list of tuples
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = (route, date, daytype, rides)
+            records.append(record)
+    return records
+
+def read_rides_as_dict(filename):
+    '''
+    Read the bus ride data as a list of dictionary
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = {
+                'route': route,
+                'date': date,
+                'daytype': daytype,
+                'rides': rides,
+            }
+            records.append(record)
+    return records
+
+class Row:
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+def read_rides_as_class(filename):
+    '''
+    Read the bus ride data as a list of class
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = Row(route, date, daytype, rides)
+            records.append(record)
+    return records
+
+import typing
+class RowNamedTuple(typing.NamedTuple):
+    route: str
+    date: str
+    daytype: str
+    rides: int
+
+def read_rides_as_namedtuples(filename):
+    '''
+    Read the bus ride data as a list of namedtuples
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = RowNamedTuple(route, date, daytype, rides)
+            records.append(record)
+    return records
+
+class RowWithSlot:
+    __slots__ = ['route', 'date', 'daytype', 'rides']
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+def read_rides_as_classwithslot(filename):
+    '''
+    Read the bus ride data as a list of class with __slots__
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = RowWithSlot(route, date, daytype, rides)
+            records.append(record)
+    return records
+
+if __name__ == '__main__':
+    func = read_rides_as_classwithslot
+
+    import tracemalloc
+    tracemalloc.start()
+    rows = func('Data/ctabus.csv')
+    print(f'Function: {func.__name__} Memory Use: Current %d, Peak %d' % tracemalloc.get_traced_memory())
